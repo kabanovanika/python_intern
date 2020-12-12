089cf85b6d79f7b7e85024ffa47215c657b96aae
Nika Kabanova <kabanovasnika@gmail.com>
2020-12-12 16:20:22 +0300

    Tests are written

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import requests
 from fastapi import FastAPI
 
-
-
 app = FastAPI()
 
 
 def is_alive_host(hostname):
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
     try:
-        r = requests.head(f'https://{hostname}')
-        if 100 <= r.status_code < 400:
-            return 'up'
-        else:
-            return 'down'
+        r = requests.get(f'https://{hostname}')
+        return 100 <= r.status_code < 400
     except requests.ConnectionError:
         message = "Failed to connect"
         return message
@@ -21,5 +16,10 @@ def is_alive_host(hostname):
 
 @app.get("/healthz")
 async def read_item(hostname: str):
-    item = {'status': is_alive_host(hostname)}
+    resp = "up" if is_alive_host(hostname) else "down"
+    item = {'status': resp}
     return item
+
+
+if __name__ == '__main__':
+    is_alive_host(1)
--- tests.py
+++ tests.py
@@ -1,17 +1,26 @@
-"""
-app tests example
-
-"""
+import requests
 
 from app import is_alive_host
 
 
-def test_alive_host():
-    pass
+class MockResponse:
+    def __init__(self, status_code):
+        self.status_code = status_code
 
 
-def test_down_host():
-    pass
+class MockGet:
+    def __init__(self, status_code):
+        self.status_code = status_code
 
-    
-/// etc...
+    def __call__(self, *args, **kwargs):
+        return MockResponse(self.status_code)
+
+
+def test_up_host(monkeypatch):
+    monkeypatch.setattr(requests, "get", MockGet(200))
+    assert is_alive_host('???')
+
+
+def test_down_host(monkeypatch):
+    monkeypatch.setattr(requests, "get", MockGet(400))
+    assert not is_alive_host('???')
