fa5671e9888367fbbbe83d02eae003a944792a96
Nika Kabanova <kabanovasnika@gmail.com>
2020-12-12 11:53:19 +0300

    Get is_alive_host working

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-"""
-app example
-
-"""
-
+import requests
 
 def is_alive_host(hostname):
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
-    pass
+    try:
+        r = requests.head(hostname)
+        print(r.status_code)
+    except requests.ConnectionError:
+        print("Failed to connect")
+
+
+
+if __name__ == '__main__':
+    is_alive_host("https://semrush.com")
+
